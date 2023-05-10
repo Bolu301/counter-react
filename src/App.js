e8501cee3cda9b8dd9c43b08ecfd630af467351c
Boluwatife Olure <105719356+Bolu301@users.noreply.github.com>
2023-05-10 11:08:00 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="header text-center">
+      <h1 className="text-4xl ">Counter Component</h1>
+      <h2 className="text-2xl my-3"> {count} </h2>
+      <div className="space-x-4">
+        <button
+          className="rounded-full bg-blue-500 py-1 px-3"
+          onClick={() => setCount(count + 1)}
+        >
+          Increment
+        </button>
+        <button
+          className="rounded-full bg-blue-500 py-1 px-3"
+          onClick={() => setCount(count - 1)}
+        >
+          Decrement
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
