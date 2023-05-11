2d0dde61a0454922525a04dee6ee85788d0ae555
Bolu301 <boluolure@gmail.com>
2023-05-11 11:58:06 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import Button from "./Button";
+// import Button from "./Button";
 import Timer from "./timer";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
