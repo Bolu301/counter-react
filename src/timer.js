d3a567710975839f732e55d87b4e27365e49e50c
Bolu301 <boluolure@gmail.com>
2023-05-11 12:21:13 +0100

    changes

--- src/timer.js
+++ src/timer.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 
-const Timer = ({ time }) => {
+const Timer = () => {
   const [time, setTime] = useState(0);
   const [running, setRunning] = useState(false);
 
